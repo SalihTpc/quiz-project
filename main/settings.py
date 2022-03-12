e8bc08a7788926e4a8eb143514535f382bee3bec
SalihTpc <salihtopcu@hotmail.com.tr>
2022-03-12 14:19:50 +0300

    some little changes

--- main/settings.py
+++ main/settings.py
@@ -131,12 +131,12 @@ OLD_PASSWORD_FIELD_ENABLED = True
 
 REST_AUTH_SERIALIZERS = {
     'TOKEN_SERIALIZER': 'users.serializers.CustomTokenSerializer',
-    
 }
 
 REST_FRAMEWORK = {
 
 'DEFAULT_AUTHENTICATION_CLASSES': ['rest_framework.authentication.TokenAuthentication',],
+# 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
 # 'DEFAULT_PERMISSION_CLASSES': ( 'rest_framework.permissions.IsAdminUser', ),
     
 }
